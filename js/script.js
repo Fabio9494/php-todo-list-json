9ced27b6b6cf633d29aff80af085ee1cabd235ce
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-29 21:26:16 +0100

    add element to to-do list

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             apiUrl: 'server.php',
+            nuovoElemento: '',
             todoList: []
         }
     },
@@ -11,6 +12,19 @@ createApp({
         this.getTodoList();
     },
     methods: {
+        updateTodoList() {
+            const data = {
+                nuovoElemento: this.nuovoElemento
+            }
+
+            axios.post(this.apiUrl, data, {
+                headers: { 'Content-type': 'multipart/form-data' }
+            }).then((response) => {
+                this.nuovoElemento = '';
+                this.todoList = response.data
+            })
+        },
+
         getTodoList() {
             axios.get(this.apiUrl).then((response) => {
                 console.log(response.data);
