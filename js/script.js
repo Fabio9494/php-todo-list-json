91289fad99fc20c9bbb770d9b1c65b13e5ba492d
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-29 22:09:18 +0100

    add bonus

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ createApp({
             const data = {
                 nuovoElemento: this.nuovoElemento
             }
-
             axios.post(this.apiUrl, data, {
                 headers: { 'Content-type': 'multipart/form-data' }
             }).then((response) => {
@@ -30,6 +29,25 @@ createApp({
                 console.log(response.data);
                 this.todoList = response.data;
             });
+        },
+        check(i) {
+            if (!this.todoList[i].done) {
+                this.todoList[i].done = true;
+            }
+            else {
+                this.todoList[i].done = false;
+            }
+        },
+        removeItem(i) {
+            const data = {
+                todoList_index: i
+            }
+            axios.post(this.apiUrl, data,
+                {
+                    headers: { 'Content-Type': 'multipart/form-data' }
+                }).then(response => {
+                    this.todoList = response.data;
+                })
         },
     },
 }).mount('#app');
--- server.php
+++ server.php
@@ -11,9 +11,24 @@
         array_push($list,$nuovoElemento);
 
         file_put_contents('todo-list.json', json_encode($list));
+
+        header('Content-type: application/json');
+
     };
 
-    header('Content-type: application/json');
+    if (isset($_POST['todoList_index'])) {
+        $todoList_index = $_POST['todoList_index'];
+    
+        $json_todoList = file_get_contents('todo-list.json');
+        $todoList = json_decode($json_todoList);
+    
+        array_splice($todoList, $todoList_index, 1);
+    
+        $json_todoList = json_encode($todoList);
+        file_put_contents('todo-list.json', $json_todoList);
+    
+        header("Content-Type: application/json");
+    }
 
     echo json_encode($list);
 ?>
